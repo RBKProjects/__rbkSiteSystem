8c2af13a6ff459cd8598efa6a5023bc0a0247c02
Majd Eddin <almuhder@gmail.com>
2017-04-12 16:45:45 +0300

    (feat) update user after the login

--- client/app/updateInfo/update.js
+++ client/app/updateInfo/update.js
@@ -1,10 +1,17 @@
 angular.module('rbkSiteSystem.update', [])
+.controller('updateController', function ($scope, User, $location, $window) {
 
-.controller('updateController', function ($scope, User) {
-	// $scope.user.id = "58eb7d1666fdb12174414540" ;
-	$scope.updateCandidateInfo = function () {
-		User.update($scope.user).then((data)=> {
-			console.log(data)
-		})
-	}
+		$scope.updateCandidateInfo = function () {
+
+				if ($window.localStorage['token']) {
+            $scope.user.id = $window.localStorage['id'];
+            User.update($scope.user).then((data) => {
+                console.log(data)
+            })
+        } else {
+            console.log("log in plz");
+            $location.path('/signin/');
+            $window.location.reload();
+        }
+    }
 });
